616d6726561529df63d0ba155fe01b48b8227535
vzarate26 <vzarate26@gmail.com>
2021-08-09 21:57:26 -0400

    add css file to Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import styles from './NavBar.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
 	return (
-		<nav>
+		<nav className={styles.container}>
 			<div>
 			{user ? (
 				<ul>
